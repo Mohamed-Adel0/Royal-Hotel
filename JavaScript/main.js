6d2083b20bbccd666ec132feb2ea41f7547f5761
Mohamed-Adel97 <128996656+Mohamed-Adel97@users.noreply.github.com>
2023-08-31 23:16:37 +0300

    first commit

---
+++ JavaScript/main.js
@@ -0,0 +1,103 @@
+
+
+let dataApi = 0
+async function Menu (){
+    let inforApi= await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=sport&apiKey=0bcb3e7c83b4412c9e0b4b24ae3481d2`)
+    dataApi = (await inforApi.json()).articles
+    display()
+}    
+Menu()
+
+async function display (){
+    
+    let cartoon =``
+    for (let i = 0; i < dataApi.length; i++) {
+        
+        if (dataApi[i].urlToImage !== null && dataApi[i].title && dataApi[i].description) {
+            cartoon +=`
+            
+            <div class="card">
+            
+          <div class="box-img">  
+            <img class="hover-img" src="${dataApi[i].urlToImage}" alt="Error">
+          </div>  
+          
+          <h3>${dataApi[i].title.split(" ").splice(0,3).join(" ")}</h3>
+          <p>${dataApi[i].description.split(" ").splice(0,4).join(" ")}</p>
+          <a class="left" href="${dataApi[i].url}">Click Here</a>
+         </div>
+         ` 
+        } 
+        document.getElementById("api").innerHTML= cartoon
+    }    
+    
+}  
+// 2nd Data in Api
+
+let api = 0
+
+async function Sport (){
+    let xdata = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0bcb3e7c83b4412c9e0b4b24ae3481d2`)
+    api = (await xdata.json()).articles
+    dataType()
+}
+Sport()
+async function dataType (){
+    paragraph =``
+    for (let i = 0; i < api.length; i++) {
+    if (api[i].urlToImage !== null) {
+        
+        paragraph +=`
+        <div class="card">
+            
+          <div class="box-img">  
+            <img src="${api[i].urlToImage}" alt="Error">
+        
+          </div>  
+          <div class="paragraph">
+          <h3>${api[i].author}</h3>
+            <p>${api[i].title.split(" ").splice(0,4).join(" ")}</p>
+            <a class="left" href="${api[i].url}">Click Here</a>
+            </div>
+         </div>
+        `
+    } 
+        document.getElementById("dataType").innerHTML =paragraph
+}
+}
+let y = "Im FrontEnd Develepoer"
+console.log(y)
+
+//---------------End API -----------------
+
+//----------Event Menu------------
+let header = document.getElementById("header")
+let nav = document.getElementById("nav")
+window.addEventListener("scroll" , navScroll)
+function navScroll(mo){
+    let scrollx = scrollY
+    // console.log(scrollx)
+    if (scrollx > 50) {
+        nav.style.width="100%"
+        header.style.position="fixed"
+        header.style.transition="1s"
+    }else{
+        nav.style.width="85%"
+        header.style.position="relative"
+
+    }
+}
+
+let xmenu = document.getElementById("menu")
+let links = document.getElementById("links")
+
+xmenu.addEventListener("click", changeData)
+
+function changeData(mo){
+    if (mo.target.className==("fa-solid fa-bars")) {
+        links.style.display="flex"
+        header.style.height="50%"
+    }
+  
+}
+
---
+++ JavaScript/main1.js
@@ -0,0 +1,273 @@
+// let x =document.getElementsByClassName("child")[0]
+// let xxx =document.querySelector(".childx")
+// let xbody =document.getElementsByTagName("body")[0]
+// let parent = document.querySelector(".parent")
+// console.log(parent);
+
+// window.addEventListener("click" , check)
+// function check (mo){
+//     if (mo.target.className==("child")) {
+//         x.classList.add("border")
+//     } else if(mo.target.className==("child")){
+//         x.classList.add("none")
+//     }
+// }
+
+// function time (mo){
+    
+//     let x =`
+//     <section>
+//     <div class="childx">
+//       <input type="email" name="" id="" placeholder="email">
+//       <input type="password" name="" id="" placeholder="password">
+//     </div>
+//   </section>`
+//     xbody.innerHTML+=x
+// }
+
+
+// setTimeout(() => {
+//     // time()
+// }, 3000);
+// parent.addEventListener("click" , color)
+// function color (){
+//     let random = Math.round(Math.random () *100)
+//     console.log(random);
+//     parent.style.backgroundColor= `hsl(${Math.round(Math.random () *360)}, ${random}%, ${random}%)`
+
+// }
+
+// color()
+
+
+
+
+// console.log(random);
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// function checkUser (mo){
+//     let userr = 100
+//     if (100) {
+//         console.log("true")
+//     } if(50){
+//         console.log("falsue");
+//     }
+// }
+// checkUser()
+
+
+// function total (num1, num2, sign){
+//     switch (sign) {
+//         case "+":
+//             console.log(num1 + num2);
+//             break;
+//         case "-":
+//             console.log(num1 + num2);
+//             break;
+//         case "*":
+//             console.log(num1 + num2);
+//             break;
+//         case "/":
+//             console.log(num1 + num2);
+//             break;
+//         case "+":
+//             console.log(num1 + num2);
+//             break;
+    
+//         default:
+//             console.log(Error);
+
+//             break;
+//     }
+// }
+
+// total(12, 30 , `/`)
+
+
+// let given = [ 1 , 2 , 3 ,4 ,5 ,6 ,7 ,8]
+
+// function checkarray(num1 , num2 , num3){
+//     for (let i = 0; i < given.length; i++) {
+//         if (num3) {
+//             console.log(num1+num2);
+//         }
+//     }
+// }
+// checkarray(num1,num2, `+`)
+
+// let parent =document.querySelector(".parent")
+// console.log(parent);
+
+
+// parent.addEventListener("click" , test)
+// function test (mo){
+//     let card = {}
+    
+//     if (mo.target.className==("child")) {
+//         card = `
+//         <section>
+//         <div class="childx">
+//        <input type="email" name="" id="" placeholder="email">
+//     </div>
+//   </section>`
+    
+//   document.getElementsByClassName("child")[0].innerHTML += card
+// }
+// }
+// localStorage
+
+// setTimeout((mo) => {
+//     test()
+// }, 1000);
+// console.log(document.getElementsByClassName("child")[0]);
+
+// let x = 20
+
+
+// if (x==25) {
+//   console.log("Yes");
+// } else if (x==20) {
+//     console.log("Yessssssssssss");
+// } else {
+//   console.log("Not Aquel");
+// }
+
+// function mo (num1 , num2){
+//   if (num1 - num2) {
+//     console.log(num1 = num2);
+//   } else {
+    
+//   }
+// }
+
+// mo(20,10)
+
+// function x (num1 , num2 , num3)
+
+// {
+//   if (num1 > num3, num2) {
+//     console.log("True");
+// } else if(num1 < num2){
+//     console.log("Yessssssssss");
+// } else {
+//     console.log("Not Aquel");
+// }
+// }
+
+// x(2, 3 ,4 )
+
+
+
+
+// function check (welcome){
+
+//   for (let i = 0; i <= welcome; i++) {
+    
+//     console.log(i); 
+//   }
+// }
+
+// check(10)
+
+
+// let x = 5
+
+// function check (){
+//   if (x =5) {
+//       for (let i = 0; i <= x; i+=5) {
+//         console.log(i);
+//       }
+//   }
+// }
+
+
+// check(20)
+
+// function check (number){
+//   for (let i = 0; i < number; i+=2) {
+//       console.log(i);
+//   }
+// }
+
+// check(100)
+
+
+// let x = 500
+// let y = 1000
+// y = x
+
+// console.log(y);
+
+// console.log( 50 == "50"  );
+
+
+// console.log( 50 === 150  );
+
+// console.log("mohamed"=== "ahmed");
+
+// window.addEventListener("click" , user)
+
+// function user (mo){
+//     if (mo.target.className===("child")) {
+//         mo.target.classList.add("border")
+//     }
+// }
+
+// console.log( 2 ** 3);
+
+// console.log(x);
+
+// var x = 123
+
+// let x = 10;
+
+// if (x === "10") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+    
+// }
+
+// let y = 10;
+
+// function check (){
+//     // console.log(y);
+//     let y = 20;
+// }
+// check();
+
+// const arr = [1,2,3]
+// arr.length=0
+// console.log(arr);
+
+// console.log("6"+4);
+
+// let person = {
+//     area: "city"
+// }
+// console.log(person.area);
+
+
+
+
+// let user = ["one" , "two" , 1,3,2, "three" , "four" , "five"];
+
+// for (let i = 0; i < user.length; i++) {
+//     if (typeof user[i] === "number") {
+//         continue;
+//     }
+//     console.log(user[i]);
+// }
+
+  
+
+
+
